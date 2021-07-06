5c0ff7ef46e0d4e0d191fad664a9aaa0afe56dca
Mahmoud Abdelrahman <47402937+JS-player@users.noreply.github.com>
2021-07-06 15:30:54 +0200

    First Version

---
+++ script.js
@@ -0,0 +1,30 @@
+const dropMenu = $(".menu")[0]; //menu icon
+dropMenu.addEventListener("click", () => {
+    $("#navMenu").toggleClass("menuClicked");
+});
+var i = 0;
+const landingPics = ["landing-0.jpg", "landing-1.jpg", "landing-2.jpg"]; //images array
+window.onload = function() {
+    setInterval(changeImage, 5000);
+
+    function changeImage() {
+        i++;
+        if (i === 3) {
+            i = 0;
+        }
+        $(".landing").css('background-image', `url(images/${landingPics[i]})`);
+        $(".bullets li").removeClass("activeBullet");
+        $(`#bull${i}`).addClass("activeBullet");
+
+    }
+}
+
+
+$(".shuffle")[0].addEventListener("click", (clickedli) => { //active filterd li
+
+    var clicked = clickedli.path[0].localName;
+    if (clicked.includes("li")) {
+        $(".shuffle li").removeClass("actived");
+        clickedli.target.className += "actived";
+    }
+});
